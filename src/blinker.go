254a09427a87671052186875eb1cd316b40a64e9
Keiji Suzuki <thegooner19@gmail.com>
2016-11-23 01:07:40 -0500

    Added blinker.go

---
+++ src/blinker.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio"
+	"net/http"
+	"os"
+)
+
+var msg = "<html><body>press here <form action=\"/green\"><input type=\"submit\" value=\"green\"></form><form action=\"/red\"><input type=\"submit\" value=\"red\"></form></body></html>"
+var greenmsg = "<html><body>Green Pressed<br/><form action=\"/green\"><input type=\"submit\" value=\"green\"></form><form action=\"/red\"><input type=\"submit\" value=\"red\"></form></body></html>"
+var redmsg = "<html><body>Red Pressed<br/><form action=\"/green\"><input type=\"submit\" value=\"green\"></form><form action=\"/red\"><input type=\"submit\" value=\"red\"></form></body></html>"
+
+var (
+	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+	greenpin = rpio.Pin(10)
+	redpin   = rpio.Pin(24)
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, msg)
+}
+
+func Green(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, greenmsg)
+	fmt.Println("green pressed")
+
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+	greenpin.High()
+	redpin.Low()
+
+}
+
+func Red(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, redmsg)
+
+	fmt.Println("red pressed")
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+	redpin.High()
+	greenpin.Low()
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/green", Green)
+	http.HandleFunc("/red", Red)
+	http.ListenAndServe(":8080", nil)
+}
